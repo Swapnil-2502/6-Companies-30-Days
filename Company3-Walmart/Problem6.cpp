933a82d0257141fcd8a78217e230133d1d24f838
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-01-15 16:05:03 +0530

    Company3 Problem6 done

---
+++ Company3-Walmart/Problem6.cpp
@@ -0,0 +1,27 @@
+396. Rotate Function
+Link: https://leetcode.com/problems/rotate-function/description/
+
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n=nums.size();
+        int product=0;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            product += nums[i]*i;
+            sum += nums[i];
+        }
+
+        int maxi = product;
+
+        for(int i=1;i<n;i++){
+            int temp = product + sum - nums[n-i] * n;
+            product = temp;
+            maxi = max(maxi,product);
+        }
+        return maxi;
+    }
+};
+//TC = O(N);
+//SC = O(1);
